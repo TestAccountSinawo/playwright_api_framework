39b5fed1ed1a1389c99067a24d5fddf175ae6dfb
Sinawo Qhaji <sinawoqhaji@Sinawos-Air.lan>
2024-09-15 20:18:05 +0200

    initial commit

---
+++ tests/api_test.spec.js
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+
+const CAPITAL = 'pretoria';
+const sasl = 'South African Sign Language';
+const baseURL = 'https://restcountries.com/v3.1';
+
+test('API Get Request', async function ({ request }) {
+
+    // Create a context that will issue http requests.
+    // Act
+    const response = await request.get(baseURL + `/capital/${CAPITAL}`);
+
+    // Assert
+    expect(response.status()).toBe(200);
+    const responseText = await response.text();
+    expect(responseText).toContain("South Africa");
+    console.log(await response.json());
+})
+
+
+test('An API to POST South African Sign Language as one of the official langues in SA', async function ({ request }) {
+
+    // Arrange
+    const response = await request.post(baseURL + `/capital/${CAPITAL}`, {
+        headers: {
+            'Accept': 'application/json'
+        },
+        data: {
+            "languages": {
+                "sasl": "South African Sign Language"
+            }
+        }
+    });
+
+    // Assert
+    expect(response.status()).toBeTruthy();
+    const responseText = await response.text();
+    expect(responseText).toContain(sasl);
+    console.log(await response.json());
+})
